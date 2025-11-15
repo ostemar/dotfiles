884e77801496323002ae5fead68db91b7038479e
Martin Östemar <martin.ostemar@visma.com>
2025-11-15 15:13:08 +0100

    Center cmd input vertically

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "40%", -- center vertically
+        },
+      },
+    },
+  },
+}
