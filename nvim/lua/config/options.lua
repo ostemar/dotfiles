1c7ec4a434094b23031e7ded0359dedd3908a89b
Martin Östemar <martin.ostemar@visma.com>
2025-11-23 09:51:08 +0100

    Set pwsh as shell on windows

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,19 @@ vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+--OS detection
+local mySysname = vim.loop.os_uname().sysname
+local isMac = mySysname == "Darwin"
+local isLinux = mySysname == "Linux"
+local isWin = mySysname:find("Windows") and true or false
+local isWsl = isLinux and vim.loop.os_uname().release:find("Microsoft") and true or false
+
+--Set shell
+if isWin then
+  if vim.fn.executable("pwsh") == 1 then
+    vim.opt.shell = "pwsh" --"pwsh" for 7.x if installed
+  else
+    vim.opt.shell = "powershell" --"powershell" for 5.x
+  end
+end
