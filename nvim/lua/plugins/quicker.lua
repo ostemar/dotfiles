dcb428613a402ace8e41c082ec8737ff5c1e4429
Martin Östemar <martin.ostemar@visma.com>
2025-11-26 10:54:21 +0100

    Configure quicker keymaps in nvim

--- nvim/lua/plugins/quicker.lua
+++ nvim/lua/plugins/quicker.lua
@@ -2,6 +2,63 @@ return {
   {
     "stevearc/quicker.nvim",
     ft = "qf",
-    opts = {},
+    opts = {
+      -- You can put configuration options here if you want
+    },
+    keys = {
+      -- These mappings work inside the quickfix (qf) window
+      {
+        "<leader>qo",
+        function()
+          require("quicker").expand()
+        end,
+        ft = "qf",
+        desc = "Quicker: expand item",
+      },
+      {
+        "<leader>qc",
+        function()
+          require("quicker").collapse()
+        end,
+        ft = "qf",
+        desc = "Quicker: collapse item",
+      },
+      {
+        "<leader>qa",
+        function()
+          require("quicker").toggle()
+        end,
+        ft = "qf",
+        desc = "Quicker: toggle expand/collapse",
+      },
+
+      -- Jump between entries (uses quicker’s smarter movement)
+      {
+        "]q",
+        function()
+          require("quicker").next()
+        end,
+        ft = "qf",
+        desc = "Quicker: next item",
+      },
+      {
+        "[q",
+        function()
+          require("quicker").prev()
+        end,
+        ft = "qf",
+        desc = "Quicker: previous item",
+      },
+
+      -- Open preview window
+      {
+        "p",
+        function()
+          require("quicker").preview()
+        end,
+        ft = "qf",
+        desc = "Quicker: preview",
+      },
+    },
   },
 }
