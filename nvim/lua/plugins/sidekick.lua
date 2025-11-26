98818040c80efe864c6eeaf528b7006fe34163ad
Martin Östemar <martin.ostemar@visma.com>
2025-11-26 10:54:02 +0100

    Temporarily disable copilot and sidekick keymaps since they collid

--- nvim/lua/plugins/sidekick.lua
+++ nvim/lua/plugins/sidekick.lua
@@ -5,4 +5,24 @@ return {
       mux = { enabled = false }, -- Windows-safe, disables zellij/tmux
     },
   },
+  --   keys = {
+  --     -- nes is also useful in normal mode
+  --     { "<tab>", LazyVim.cmp.map({ "ai_nes" }, "<tab>"), mode = { "n" }, expr = true },
+  --     { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+  --     {
+  --       "<c-.>",
+  --       function()
+  --         require("sidekick.cli").toggle()
+  --       end,
+  --       desc = "Sidekick Toggle",
+  --       mode = { "n", "t", "i", "x" },
+  --     },
+  --     {
+  --       "<leader>aa",
+  --       function()
+  --         require("sidekick.cli").toggle()
+  --       end,
+  --       desc = "Sidekick Toggle CLI",
+  --     },
+  --   },
 }
